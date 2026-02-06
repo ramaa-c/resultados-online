0bd09fef591d4f9a9d6c848fcc7ebc8f23d50b2b
ramaa-c <raramiro.240@gmail.com>
2026-02-06 14:35:37 -0300

    comentarios

--- src/services/protocols.service.js
+++ src/services/protocols.service.js
@@ -15,21 +15,15 @@ export const getProtocols = async (filters) => {
   }
   delete processedFilters.apellido_paciente;
 
-Object.entries(processedFilters).forEach(([key, value]) => {
-    // 1. Verificamos que el valor exista (que no sea null ni undefined). 
-    // OJO: 'false' pasa esta validación, lo cual es correcto.
+  Object.entries(processedFilters).forEach(([key, value]) => {
     if (value !== undefined && value !== "" && value !== null) {
       let valorFinal = value;
 
       if (key === "unread_only" || key === "complete_only") {
-        // 2. CORRECCIÓN CLAVE:
-        // Eliminamos el 'if (value === true)' para que no discrimine a los falsos.
-        // Convertimos el booleano a string ("true" o "false") y lo enviamos.
-        params.append(key, String(value)); 
-        return; // Salimos aquí porque ya lo agregamos
+        params.append(key, String(value));
+        return;
       }
 
-      // Lógica para fechas
       if (
         (key === "date_from" || key === "date_to") &&
         typeof value === "string"
@@ -37,7 +31,6 @@ Object.entries(processedFilters).forEach(([key, value]) => {
         valorFinal = value.replaceAll("-", "");
       }
 
-      // Para el resto de filtros
       params.append(key, valorFinal);
     }
   });
